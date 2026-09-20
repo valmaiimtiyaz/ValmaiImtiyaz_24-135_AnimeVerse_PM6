f1d48e4d91d2e128b8adb75a0edfa2f1b88b955b
valmaiimtiyaz <valmaityz30@gmail.com>
2026-09-20 18:30:44 +0700

    menambahkan models anime

---
+++ lib/models/anime.dart
@@ -0,0 +1,19 @@
+class Anime {
+  final String id;
+  final String title;
+  final String imagePath;
+  final String genre;
+  final String rating;
+  final String totalEpisodes;
+  final String description;
+
+  const Anime({
+    required this.id,
+    required this.title,
+    required this.imagePath,
+    required this.genre,
+    required this.rating,
+    required this.totalEpisodes,
+    required this.description,
+  });
+}
