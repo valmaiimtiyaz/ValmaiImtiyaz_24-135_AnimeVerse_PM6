4c717406fe67acabf2cfb693ef06f034c58c2dc7
valmaiimtiyaz <valmaityz30@gmail.com>
2026-09-20 18:27:20 +0700

    menambahkan home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/app_scaffold.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return AppScaffold(
+      appBar: AppBar(
+        title: Text(
+          "AnimeVerse",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w800,
+            fontSize: screenWidth * 0.075,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //search bar
+            Padding(
+              padding: EdgeInsets.all(screenWidth * 0.04),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(screenWidth * 0.075),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withValues(alpha: 0.3),
+                      blurRadius: screenWidth * 0.02,
+                      offset: Offset(0, screenHeight * 0.005),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Anime Title",
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: screenWidth * 0.04,
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                      size: screenWidth * 0.06,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(screenWidth * 0.075),
+                      borderSide: BorderSide.none,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(screenWidth * 0.075),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(screenWidth * 0.075),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    filled: true,
+                    fillColor: Color(0xFF0b395e),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: screenWidth * 0.05,
+                      vertical: screenHeight * 0.015,
+                    ),
+                  ),
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.04,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
