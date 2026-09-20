fe03f9194d31f1e00dd22386442cbf197aef1691
valmaiimtiyaz <valmaityz30@gmail.com>
2026-09-20 22:49:29 +0700

    update detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -19,7 +19,7 @@ class DetailScreen extends StatelessWidget {
     this.genre = 'Action, Adventure, Fantasy',
     this.rating = '8.14',
     this.totalEpisodes = '170',
-    this.description = "",
+    this.description = "Asta and Yuno were abandoned at the same church on the same day. Raised together as children, they came to know of the 'Wizard King'—a title given to the strongest mage in the kingdom—and promised that they would compete against each other for the position of the next Wizard King. However, as they grew up, the stark difference between them became evident. While Yuno is able to wield magic with amazing power and control, Asta cannot use magic at all and desperately tries to awaken his powers by training physically. When they reach the age of 15, Yuno is bestowed a spectacular Grimoire with a four-leaf clover, while Asta receives nothing. However, soon after, Yuno is attacked by a person named Lebuty, whose main purpose is to obtain Yuno's Grimoire. Asta tries to fight Lebuty, but he is outmatched. Though without hope and on the brink of defeat, he finds the strength to continue when he hears Yuno's voice. Unleashing his inner emotions in a rage, Asta receives a five-leaf clover Grimoire, a 'Black Clover' giving him enough power to defeat Lebuty. A few days later, the two friends head out into the world, both seeking the same goal—to become the Wizard King! [Written by MAL Rewrite]",
   });
 
   @override
@@ -255,9 +255,7 @@ class DetailScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-
                   SizedBox(height: screenHeight * 0.015),
-
                   Text(
                     description,
                     style: TextStyle(
@@ -267,7 +265,6 @@ class DetailScreen extends StatelessWidget {
                     ),
                     textAlign: TextAlign.justify,
                   ),
-
                   SizedBox(height: screenHeight * 0.03),
                 ],
               ),
